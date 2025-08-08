8a3764f388f52074f43d43a5f715d5410921746f
jwsiejk <jwsiejk@gmail.com>
2025-08-07 21:58:38 -0400

    Update app.py

--- app.py
+++ app.py
@@ -14,9 +14,15 @@ from datetime import datetime
 
 def get_connection():
     import psycopg2
+    import urllib.parse as up
     db_url = os.getenv("DATABASE_URL")
+
     if not db_url:
         raise ValueError("DATABASE_URL is not set in environment.")
+
+    if "sslmode" not in db_url:
+        db_url += "?sslmode=require"
+
     return psycopg2.connect(db_url)
 
 app = Flask(__name__)
