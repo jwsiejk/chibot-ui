eadda0c181dedd4fa488d7d11d3a40d95ae0d9d1
jwsiejk <jwsiejk@gmail.com>
2025-09-01 20:47:14 -0400

    Update config.js

--- static/user-experience/js/config.js
+++ static/user-experience/js/config.js
@@ -1,4 +1,3 @@
-// static/user-experience/js/config.js
-// Loaded BEFORE any other app scripts
-window.ASKCHIP_API_BASE = "https://askchip-api.onrender.com";
-window.ASKCHIP_WS_BASE  = "wss://askchip-api.onrender.com";
+window.ASKCHIP_API_BASE = ""; // same-origin
+window.ASKCHIP_WS_BASE  = ""; // same-origin
+
