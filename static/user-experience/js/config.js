655a4460f6a0d81a353cbfd29ca1ec3edd9be506
jwsiejk <jwsiejk@gmail.com>
2025-09-01 21:35:18 -0400

    Update config.js

--- static/user-experience/js/config.js
+++ static/user-experience/js/config.js
@@ -1,3 +1,4 @@
+// static/user-experience/js/config.js
 window.ASKCHIP_API_BASE = ""; // same-origin
 window.ASKCHIP_WS_BASE  = ""; // same-origin
-
+window.__ASKCHIP_API_ORIGIN = ""; // keep if your UI reads this
