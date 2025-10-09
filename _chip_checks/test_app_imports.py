8907b8043c3ffb8067310fb6ac7ef5ee310158f4
jwsiejk <jwsiejk@gmail.com>
2025-10-08 23:42:44 -0400

    Delete _chip_checks directory

--- _chip_checks/test_app_imports.py
+++
@@ -1,6 +0,0 @@
-import sys, pathlib
-sys.path.insert(0, str(pathlib.Path(__file__).resolve().parents[1]))
-from app import create_app
-def test_import_create_app():
-    app = create_app()
-    assert app is not None
