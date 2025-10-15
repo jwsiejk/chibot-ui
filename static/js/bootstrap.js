86271732a8b5b30c6be39a2e789b8bfbdf86f1d6
jwsiejk <jwsiejk@gmail.com>
2025-10-14 21:38:50 -0400

    Update bootstrap.js

--- static/js/bootstrap.js
+++ static/js/bootstrap.js
@@ -351,6 +351,7 @@ async function startOnce(){
     //    audio hardware comes online.
     const sid = getSID();
     try {
+      try { setGreetGateActive(true); } catch {}      
       const manualMode = !!_cfgValue('barge_in_mode_manual', true);
       const autoCommit = !!_cfgValue('auto_commit_when_ready', true);
       _console('log', '[bootstrap] startOnce sending greet configure');
@@ -450,6 +451,7 @@ async function startOnce(){
     _disableButtons();
     setDot('ready');
     try { Visualizer.stop({ reset: true }); } catch {}
+    try { setGreetGateActive(false); } catch {}    
   } finally {
     startInFlight = false;
   }
